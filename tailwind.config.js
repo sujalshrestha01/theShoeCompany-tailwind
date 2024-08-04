c7aa40a55fade793d49e765011901eb963180380
Sujal Shrestha <sujal8696@gmail.com>
2024-08-04 13:12:01 +0545

    initial-hero section

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+// npx tailwindcss -i ./input.css -o ./output.css --watch
